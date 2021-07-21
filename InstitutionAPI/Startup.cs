b9430620af3c0e124ded58800cf0a235f4d49729
Justin <justinmann.mail@gmail.com>
2021-07-20 21:50:16 -0600

    .

--- InstitutionAPI/Startup.cs
+++ InstitutionAPI/Startup.cs
@@ -2,18 +2,11 @@ using InstitutionAPI.Config;
 using InstitutionAPI.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using ResumeInfastructure.CosmosDbData.Extension;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace InstitutionAPI {
     public class Startup {
